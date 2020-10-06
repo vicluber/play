0b5a8856e70b795044deb4308e18f9df99e92aa9
Victor Willhuber <victorwillhuber@gmail.com>
2020-10-06 00:19:40 -0300

    Working, nexts steps, menu scene, and game over scene. (change variables for proper names, descriptive names)

--- main.js
+++ main.js
@@ -24,48 +24,62 @@ var config = {
 var theLetter;
 var sun;
 var game = new Phaser.Game(config);
-
+var misilesCleared = 0;
+var text;
 function preload ()
 {
-    this.load.image('sun', 'sun.png');
-    this.load.bitmapFont('desyrel', 'assets/bitmapFonts/desyrel.png', 'assets/bitmapFonts/desyrel.xml');
+    this.load.image('misile', 'assets/misile.png');
+    this.load.image('bg-sky', 'assets/bg-sky.png');
+    this.load.image('bg-city', 'assets/bg-city.png');
+    this.load.bitmapFont('shortStack', 'assets/bitmapFonts/shortStack.png', 'assets/bitmapFonts/shortStack.xml');
 }
-var text;
+
 function create ()
 {
-    text = this.add.bitmapText(200, 100, 'desyrel','',24);
+    this.add.image(0, 0, 'bg-sky').setOrigin(0, 0);
+    this.add.image(0, 600-241, 'bg-city').setOrigin(0, 0);
+    text = this.add.bitmapText(200, 100, 'shortStack','',24);
+    totalMisiles = this.add.text(32, 32);
+    totalMisiles.text = '0';
+    
     theLetter = generateRandomLetter();
     text.text = theLetter;
-    sun = this.physics.add.sprite(this.sys.game.config.width/2,0,"sun");
+    sun = this.physics.add.sprite(WIDTH/2, HEIGHT, "misile");
     sun.setGravityY(100);
-    sun.setScale(0.1);
+    sun.setScale(0.5);
     sun.setGravityY(10);
     sun.setCollideWorldBounds(true);
     sun.body.world.checkCollision.up = false;
+    console.log(text);
+    console.log(totalMisiles);
 }
 function generateRandomLetter()
 {
-    const alphabet = "abcdefghijklmnopqrstuvwxyz"
-    return alphabet[Math.floor(Math.random() * alphabet.length)]
+    const alphabet = "abcdefghijklmnopqrstuvwxyz" //defining the characters able to chose randomly
+    return alphabet[Math.floor(Math.random() * alphabet.length)] //return a character from "alphabet" on position equal to random between amount of characters on "alphabet"
   }
 function update()
 {
-    text.x = sun.x + 5;
-    text.y = sun.y;
-    if(sun.body.blocked.down)
+    text.x = sun.x + 15; //Updating text position to follow the letter object (plus 5 so is not exactly on top of it)
+    text.y = sun.y + 20; //Updating text position to follow the letter object
+    if(sun.body.blocked.down) //if the letter objetct touchs the bottom (world boundrie)
     {
-        sun.y = -30;
-        sun.x = Math.floor(Math.random() * WIDTH);
+        misilesCleared = 0; //you lose score goes to cero
+        totalMisiles.text = misilesCleared.toString(); //updateing score text
+        sun.y = -30; //takes the letter object back to top to set the next letter
+        sun.x = Math.floor(Math.random() * WIDTH - 30); //Random x position for the next letter
     }
     
-    window.onkeydown = function(e)
+    window.onkeydown = function(e) //when windows detects event (onkeydown)
     {
-        if(e.key == theLetter)
+        if(e.key == theLetter) //if the "keydown" is equal to "the letter"
         {
-            theLetter = generateRandomLetter();
-            text.text = theLetter;
-            sun.y = -30;
-            sun.x = Math.floor(Math.random() * WIDTH);
+            misilesCleared++; //Counting misiles cleared (score)
+            totalMisiles.text = misilesCleared.toString(); //updateing score text
+            theLetter = generateRandomLetter(); //asigning new random letter to "theLettter" for next letter
+            text.text = theLetter; //setting the text to text object in "theLetter" as above
+            sun.y = -30; //setting object position to the top of the viewport minux 30 so is hide from it and it fills like a new one
+            sun.x = Math.floor(Math.random() * WIDTH - 30); //Random x position for the next letter
         }
     }
 }
