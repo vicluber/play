2f6c8d248c9dc54dd951baa2397b7b453d9a3986
Victor Willhuber <victorwillhuber@gmail.com>
2020-10-07 21:12:07 -0300

    [FEATURE] Adding portal

--- main.js
+++ main.js
@@ -28,10 +28,12 @@ var misilesCleared = 0;
 var text;
 var lives = 3;
 var hearts;
+var portal;
 function preload ()
 {
     this.load.image('misile', 'assets/misile.png');
     this.load.image('bg-sky', 'assets/bg-sky.png');
+    this.load.image('portal', 'assets/portal.png');
     this.load.image('hart-points', 'assets/hart.png');
     this.load.image('dead-hart', 'assets/dead-hart.png');
     this.load.image('bg-city', 'assets/bg-city.png');
@@ -40,29 +42,28 @@ function preload ()
 
 function create ()
 {
+    //Scenariography
     this.add.image(0, 0, 'bg-sky').setOrigin(0, 0);
-    //hartLives = this.add.image(WIDTH - 50, 20, 'hart-points');
     this.add.image(0, 900-241, 'bg-city').setOrigin(0, 0);
+    //Seting text for misile
     text = this.add.text(32, 32);
     text.setFontSize(25)
+    //Setting text for amount of misiles cleared
     totalMisiles = this.add.text(32, 32);
     totalMisiles.setFontSize(40);
     totalMisiles.text = '0';
-    
+    //Generating randome letter for setting on the next misile text
     theLetter = generateRandomLetter();
     text.text = theLetter;
+    //Creating and setting the first missile
     misile = this.physics.add.sprite(WIDTH/2, HEIGHT, "misile");
     misile.setGravityY(100);
     misile.setScale(0.5);
     misile.setGravityY(10);
+    portal = this.add.image(-200, -200, 'portal');
     misile.setCollideWorldBounds(true);
     misile.body.world.checkCollision.up = false;
     hearts = this.physics.add.staticGroup({ key: 'hart-points', repeat: 2, setXY: { x: WIDTH - 150, y: 32, stepX: 50 }});
-    /*stars.children.iterate(function (child) {
-    
-        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-    
-    });*/
 }
 function generateRandomLetter()
 {
@@ -102,6 +103,7 @@ function update()
         {
             misilesCleared++; //Counting misiles cleared (score)
             totalMisiles.text = misilesCleared.toString(); //updateing score text
+            
             theLetter = generateRandomLetter(); //asigning new random letter to "theLettter" for next letter
             text.text = theLetter; //setting the text to text object in "theLetter" as above
             misile.y = -30; //setting object position to the top of the viewport minux 30 so is hide from it and it fills like a new one
